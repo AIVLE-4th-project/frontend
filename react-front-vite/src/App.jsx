09304ddb96f17fa2ad9ff0d65a9c7846fa2ac0e5
sangyeop <sy3947@gmail.com>
2025-05-30 11:05:28 +0900

    router 추가가

--- react-front-vite/src/App.jsx
+++ react-front-vite/src/App.jsx
@@ -1,34 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import BookList from "./pages/BookList";
+import BookRegister from "./pages/BookRegister";
+import BookDetail from "./pages/BookDetail";
+import BookEdit from "./pages/BookEdit";
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<BookList />} />
+      <Route path="/register" element={<BookRegister />} />
+      <Route path="/book/:id" element={<BookDetail />} />
+      <Route path="/edit/:id" element={<BookEdit />} />
+    </Routes>
+  </BrowserRouter>
   )
 }
 
